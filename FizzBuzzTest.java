bd001c247cfb82f3284ac8860e41307e01dca473
Rusman313 <62064583+Rusman313@users.noreply.github.com>
2020-06-01 09:33:22 -0400

    processNumber outputs a String when given integer

---
+++ FizzBuzzTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+    @Test
+    public void test(){
+
+    }
+
+    @Test
+    public void shouldProcessInput(){// important to be descriptive so that we know what is being tested
+        FizzBuzz fizzBuzz =  new FizzBuzz();
+        //String output =  fizzBuzz.processNumber(1);// if we pass this method integer will pass as a string
+        assertEquals("1", fizzBuzz.processNumber(1));
+    }
+}
