3c7e28854dbbb8918a2905fa994070a9b5a20505
Rusman313 <62064583+Rusman313@users.noreply.github.com>
2020-06-01 10:04:27 -0400

    refactored fizzBuzz.java with private methods

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,14 +4,22 @@ public class FizzBuzz {
     }
 
     public String processNumber(int number){
-        if(number % 3 == 0 && number % 5 == 0){
+        if(isMultipleOfThree(number) && isMultipleOfFive(number)){
             return "Fizz Buzz"; // we put this first because this will be checked first, because possible numbers would be divisible by 3 and 5 it would stop at the first one 
         }
-        else if (number % 3 == 0){ // if number is divisible by 3, result is 0, return Fizz
+        else if (isMultipleOfThree(number)) { // if number is divisible by 3, result is 0, return Fizz
             return "Fizz";
-        }else if(number % 5 == 0){return "Buzz";}//// if number is divisible by 5, result is 0, return buzz
+        }else if(isMultipleOfFive(number)){return "Buzz";}//// if number is divisible by 5, result is 0, return buzz
         else{
         return Integer.toString(number);
         }
     }
+
+    private boolean isMultipleOfFive(int number){
+        return number % 5 == 0;
+    }
+    private boolean isMultipleOfThree(int number){
+        return number % 3 == 0;
+    }
+
 }
