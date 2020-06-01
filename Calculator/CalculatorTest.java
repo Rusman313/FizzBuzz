bc9b98f4a26bae1542b3029a4564e9bceb253dce
Rusman313 <62064583+Rusman313@users.noreply.github.com>
2020-06-01 11:39:52 -0400

    Throw exception if negative number is used

--- Calculator/CalculatorTest.java
+++ Calculator/CalculatorTest.java
@@ -4,37 +4,48 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
-    // use this annotation ( ) when your expecting a RuntimeException, not any kind of returned value
-    @Test(expected = RuntimeException.class)
-    public final void whenMoretThan2NumbersAreUsedThenAnExceptionIsThrown(){
-        Calculator.add("1,2,3");
+
+    // Use this annotation( ) when you're expecting a RuntimeException, not any kind
+    // of returned value
+    // @Test(expected = RuntimeException.class)
+    // public final void whenMoreThan2NumbersAreUsedThenAnExceptionIsThrown() {
+    // Calculator.add("1,2,3");
+    // }
+
+    @Test
+    public final void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
+        Assert.assertEquals(1 + 2 + 3 + 4 + 5 + 6 + 7, Calculator.add("1,2,3,4,5,6,7"));
     }
 
     @Test
-    public final void whenMoretThan2NumbersAreUsedThenNoExceptionIsThrown(){
+    public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
         Calculator.add("1,2");
         Assert.assertTrue(true);
     }
 
     @Test(expected = RuntimeException.class)
-    public final void whenNonNumbersAreUsedThenAnExceptionIsThrown(){
-        Calculator.add("1,R");
-
+    public final void whenNonNumberIsUsedThenExceptionIsThrown() {
+        Calculator.add("1,X");
     }
 
     @Test
-    public final void whenEmptyStringIsUsedThenReturnValue0(){
-        Assert.assertEquals(0,Calculator.add(""));
+    public final void whenEmptyStringIsUsedThenReturnValue0() {
+        Assert.assertEquals(0, Calculator.add(""));
     }
 
     @Test
-    public final void whenOneNumberIsUsedThenReturnValueIsSameNumber(){
-        Assert.assertEquals(3,Calculator.add("3"));
+    public final void whenOneNumberIsUsedThenReturnValueIsSameNumber() {
+        Assert.assertEquals(10, Calculator.add("10"));
     }
 
     @Test
-    public final void whenTwoNumbersAreUsedReturnValueIsTheirSum(){
-        Assert.assertEquals(3+6, Calculator.add("3,6"));
+    public final void whenTwoNumbersAreUsedReturnValueIsTheirSum() {
+        Assert.assertEquals(3 + 6, Calculator.add("3,6"));
     }
-    
+
+    @Test(expected = RuntimeException.class)
+    public final void whenNegativeNumberIsUsedThenRuntimeExceptionIsThrown() {
+        Calculator.add("3,-6,9");
+    }
+
 }
