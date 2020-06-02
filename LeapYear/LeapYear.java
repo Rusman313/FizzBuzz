27499c756940fbce1f99e031b2ed717a3e2fcc85
Rusman313 <62064583+Rusman313@users.noreply.github.com>
2020-06-01 20:28:18 -0400

    Created methods to pass tests for is leapyear, is year divisible by 4 etc.

--- LeapYear/LeapYear.java
+++ LeapYear/LeapYear.java
@@ -1,5 +1,11 @@
 package LeapYear;
 
 public class LeapYear {
-    
+  
+
+        public boolean isLeapYear(int year) {    
+          
+            if(year % 100 == 0 && year % 400 != 0 || year %4 !=0){ return false;}
+            return true;
+    } 
 }
--- LeapYear/LeapYearTest.java
+++ LeapYear/LeapYearTest.java
@@ -1,20 +1,30 @@
 package LeapYear;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class LeapYearTest {
+    LeapYear LY = new LeapYear();
     @Test
     public void isYearDivisibleBy4(){
-
+      
+        Assert.assertEquals(true, LY.isLeapYear(1996));
     }
 
     @Test
-    public void isNotYearDivisibleBy100UnlessDivisibleBy400Also(){
+    public void YearIsNotDivisibleBy100UnlessDivisibleBy400Also(){
+        Assert.assertEquals(true, LY.isLeapYear(2000));
+        Assert.assertEquals(false, LY.isLeapYear(1900));
 
     }
+
 
     @Test
     public void isALeapYear(){
-        
+        Assert.assertEquals(false, LY.isLeapYear(1900));
+        Assert.assertEquals(true, LY.isLeapYear(2000));
+        Assert.assertEquals(false, LY.isLeapYear(2001));
+        Assert.assertEquals(true, LY.isLeapYear(1996));
+
     }
 }
